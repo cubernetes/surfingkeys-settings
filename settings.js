20e707965c5ae5c5b1040d6bdc93e9e1442406ca
cubernetes <71025646+cubernetes@users.noreply.github.com>
2025-06-14 23:36:46 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,35 @@
-api.addSearchAlias('l', 'leo-ger-eng', 'https://dict.leo.org/german-english/', 's', 'https://dict.leo.org/dictQuery/m-query/conf/ende/query.conf/strlist.json?q={0}&shortQuery&noDescription&sideInfo=on&where=both&term={0}', function(response) {
-    return JSON.parse(response.text)[1];
-}, 'o', options={favicon_url: 'https://dict.leo.org/img/favicons/leo-32.png'});
+api.addSearchAlias(
+	'l',
+	'leo-ger-eng',
+	'https://dict.leo.org/german-english/',
+	's',
+	'https://dict.leo.org/dictQuery/m-query/conf/ende/query.conf/strlist.json?q={0}&shortQuery&noDescription&sideInfo=on&where=both&term=',
+	resp => JSON.parse(resp.text)[1],
+	'o',
+	options={favicon_url: 'https://dict.leo.org/img/favicons/leo-32.png'}
+);
 
-api.addSearchAlias('v', 'google-verbatim', 'https://www.google.com/search?tbs=li:1&q=%22{0}%22', 's', 'https://www.google.com/complete/search?client=chrome-omni&gs_ri=chrome-ext&oit=1&cp=1&pgcl=7&tbs=li:1&q=', function(response) {
-    var res = JSON.parse(response.text);
-    return res[1];
-});
+api.addSearchAlias(
+	'v',
+	'google-verbatim',
+	'https://www.google.com/search?tbs=li:1&q=%22{0}%22',
+	's',
+	'https://www.google.com/complete/search?client=chrome-omni&gs_ri=chrome-ext&oit=1&cp=1&pgcl=7&tbs=li:1&q=',
+	resp => JSON.parse(resp.text)[1]
+);
+
+api.removeSearchAlias('w')
+
+api.addSearchAlias(
+	'w',
+	'wiktionary',
+	'https://en.wiktionary.org/wiki/',
+	's',
+	'https://en.wiktionary.org/w/api.php?action=query&format=json&generator=prefixsearch&gpssearch=',
+	resp => Object.values(JSON.parse(resp.text).query.pages).map(e => e.title),
+	'o',
+	options={favicon_url: 'https://de.wiktionary.org/static/favicon/piece.ico'}
+);
 
 api.iunmap('<Ctrl-a>');
 
